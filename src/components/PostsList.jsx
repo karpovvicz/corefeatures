cc579e8b893232dd22f6fd91120d4a9bfdab3b21
karpovvicz <karpovvicz@gmail.com>
2025-07-10 14:14:10 +0200

    added react router, created two main routes

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,63 +1,64 @@
-import classes from "./Post.module.css";
-import Post from "./Post.jsx";
-import NewPost from "./NewPost.jsx";
-import { useState, useEffect} from "react";
-import Modal from "./Modal.jsx";
+import { useState, useEffect } from 'react';
 
-function PostsList({isPosting, onStopPosting}) {
+import Post from './Post';
+import NewPost from './NewPost';
+import Modal from './Modal';
+import classes from './PostsList.module.css';
 
+function PostsList({ isPosting, onStopPosting }) {
+    const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
 
     useEffect(() => {
         async function fetchPosts() {
-           const response = await fetch('http//:localhost:8080/posts')
-            const resData =  await response.json();
-                    setPosts(resData.posts);
+            setIsFetching(true);
+            const response = await fetch('http://localhost:8080/posts');
+            const resData = await response.json();
+            setPosts(resData.posts);
+            setIsFetching(false);
         }
+
         fetchPosts();
     }, []);
 
-    const [posts, setPosts] = useState([]);
-
     function addPostHandler(postData) {
         fetch('http://localhost:8080/posts', {
-            method: "POST",
+            method: 'POST',
             body: JSON.stringify(postData),
             headers: {
-                "Content-Type": "application/json",
-            }
+                'Content-Type': 'application/json',
+            },
         });
         setPosts((existingPosts) => [postData, ...existingPosts]);
     }
 
     return (
         <>
-            {isPosting ? <Modal onClose={onStopPosting}>
-                <NewPost
-                    onAddPost={addPostHandler}
-
-                    onCancel={onStopPosting}
-                />
-            </Modal> : false}
-            {posts.length > 0 && (
-                <ul className={classes.post}>
-
-
+            {isPosting && (
+                <Modal onClose={onStopPosting}>
+                    <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+                </Modal>
+            )}
+            {!isFetching && posts.length > 0 && (
+                <ul className={classes.posts}>
                     {posts.map((post) => (
-                        <Post author={post.author} key={post.body} body={post.body}/>
+                        <Post key={post.body} author={post.author} body={post.body} />
                     ))}
-
                 </ul>
             )}
-            {posts.length === 0 && (
-                <div style={{ textAlign: "center", color: "white" }}>
-                    <h2>There are no posts yet</h2>
+            {!isFetching && posts.length === 0 && (
+                <div style={{ textAlign: 'center', color: 'white' }}>
+                    <h2>There are no posts yet.</h2>
                     <p>Start adding some!</p>
                 </div>
             )}
-
+            {isFetching && (
+                <div style={{ textAlign: 'center', color: 'white' }}>
+                    <p>Loading posts...</p>
+                </div>
+            )}
         </>
-    )
+    );
 }
-
 
 export default PostsList;
